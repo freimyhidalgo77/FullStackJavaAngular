188ffedfb1432028733adf65cafce6074a7844fc
freimyHidalgo77 <freimy4@gmail.com>
2026-03-14 14:42:31 -0400

    Finalizando cambios y ajustes

--- src/app/models/reservation.model.ts
+++ src/app/models/reservation.model.ts
@@ -1,13 +1,18 @@
-export enum ReservationStatus {
-  ACTIVE = 'ACTIVE',
-  CANCELLED = 'CANCELLED'
-}
+
+export type ReservationStatus = 'ACTIVe' | 'CANCELLED'; 
+
 
 export interface Reservation {
-  id: number;
+  id?: number;
   customerName: string;
   date: string;
   time: string;
   service: string;
   status: ReservationStatus;
 }
+
+
+/*export enum ReservationStatus {
+  ACTIVe = 'ACTIVE',
+  CANCELLED = 'CANCELLED'
+}*/
